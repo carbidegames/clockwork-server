397ac91a6e4f2fdd6f0d867b10634e761b57739e
Layl <LaylConway@users.noreply.github.com>
2016-07-23 06:23:35 +0200

    Remove unneeded dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,7 @@ extern crate crossbeam;
 extern crate hyper;
 #[macro_use] extern crate log;
 extern crate num_cpus;
-extern crate route_recognizer;
 extern crate webapp;
-#[macro_use] extern crate try_opt;
-#[macro_use] extern crate mopa;
 
 mod listener;
 mod server;
